200be0f22688abcbe42d82665ea52e8a30c53798
KeterTheWolf <ketertw404@gmail.com>
2016-11-30 00:26:26 -0500

    Update menubar.py
    Minor changes.

--- menubar.py
+++ menubar.py
@@ -1,20 +1,20 @@
-from tkinter import *
+sfrom tkinter import *
 from __main__ import *
 
 def blank():
    filewin = Toplevel(base)
-   button = Button(filewin, text="Do nothing button", command=filewin.destroy)
+   button = Button(filewin, text="Close", command=filewin.destroy)
    button.pack()
 
 menubar = Menu(base)
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New", command=blank)
-filemenu.add_command(label="Open", command=blank)
-filemenu.add_command(label="Save", command=blank)
-filemenu.add_command(label="Save as...", command=blank)
-filemenu.add_command(label="Close", command=blank)
+filemenu.add_command(label="1", command=blank)
+filemenu.add_command(label="2", command=blank)
+filemenu.add_command(label="3", command=blank)
+filemenu.add_command(label="4", command=blank)
+filemenu.add_command(label="5", command=blank)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=base.quit)
 
-menubar.add_cascade(label="File", menu=filemenu)
+menubar.add_cascade(label="Fambox", menu=filemenu)
 base.config(menu=menubar)
